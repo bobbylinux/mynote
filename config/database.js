45efb8698912b1f9fd2701729eb5db1c2e82cf67
EptaLab <info@eptalab.com>
2018-09-05 12:32:38 +0200

    tolto database.js

--- config/database.js
+++
@@ -1,5 +0,0 @@
-if (process.env.NODE_ENV === 'production') {
-    module.exports={mongoURI: "mongodb://bobbylinux:firenze83@ds245512.mlab.com:45512/note-prod"}
-} else {
-    module.exports={mongoURI: "mongodb://127.0.0.1:27017/note"}
-}
